75471f94425d7b5a926299d0a386b38b61a0c562
jenniferyb <jenniferyb@hotmail.com>
2016-11-12 03:43:00 -0300

    Add files via upload

---
+++ dominio/src/main/java/peticiones/PeticionCrearPersonaje.java
@@ -0,0 +1,23 @@
+package peticiones;
+
+import cs.ServerTh;
+
+public class PeticionCrearPersonaje {
+	private String respuesta;
+	
+	public String getRespuesta() {
+		return respuesta;
+	}
+
+	public PeticionCrearPersonaje(String[] datosPeticion, ServerTh serverTh) {
+	/*	String 	usuario = datosPeticion[1];
+		int 	minJ = Integer.parseInt(datosPeticion[2]),
+				maxJ = Integer.parseInt(datosPeticion[3]);
+		boolean resultado=serverTh.getServer().getConexionBD().agregarJugador(usuario, minJ, maxJ);
+		if(resultado)
+			respuesta = CodigoPeticion.CREAR_JUGADOR_CORRECTO+"";
+		else
+			respuesta = CodigoPeticion.CREAR_JUGADOR_INCORRECTO+"";
+		*/
+	}
+}
